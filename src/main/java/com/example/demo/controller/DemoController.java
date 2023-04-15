3c8207480d6585fe48e3764d575d8ca8d9679f6e
SicutLumen <SicutLumen@gmail.com>
2023-04-16 00:40:13 +0200

    feat: test project

---
+++ src/main/java/com/example/demo/controller/DemoController.java
@@ -0,0 +1,14 @@
+package com.example.demo.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    @RequestMapping("/")
+    public ResponseEntity<String> demo(){
+        return ResponseEntity.ok().body("Demo, demo");
+    }
+}
